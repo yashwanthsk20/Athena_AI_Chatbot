2392583a6328c98a0685fef5c6754b9d99b6bf32
yashvanthsk <yashwanthsk20@gmail.com>
2025-01-17 19:21:30 +0530

    reset updated

--- server/src/controllers/User/reset.js
+++ server/src/controllers/User/reset.js
@@ -1,15 +1,13 @@
-import env from "../../env/variables.js";
 import { OTP } from "../../models/otp.model.js";
 import { User } from "../../models/user.model.js";
 import ApiResponse from "../../utils/ApiResponse.js";
-import { sendEmail } from "../../utils/mailer.js";
-import { TOTP } from "totp-generator";
+import bcrypt from "bcrypt";
 
-const sendMail = async (req, res) => {
+const resetPassword = async (req, res) => {
   try {
-    const { email } = req.body;
+    const { email, otp, newPassword } = req.body;
 
-    if (!email) {
+    if (!email || !otp || !newPassword) {
       return res
         .status(400)
         .send(new ApiResponse(400, null, "Required fields missing."));
@@ -31,51 +29,38 @@ const sendMail = async (req, res) => {
 
     const existingOtp = await OTP.findOne({ email });
 
-    if (existingOtp) {
-      const { otp, expires } = TOTP.generate(env.OTP_SECRET, {
-        digits: 6,
-        period: 600,
-      }); // 10 minutes = 600 s
-
-      existingOtp.code = otp;
-      existingOtp.expiry = expires;
-      await existingOtp.save();
-
-      const content = `Dear User,<br /><br />We have received a request to reset your password for <b>Athena AI</b><br/>If this request was not made by you, you can ignore this email.<br /><br />Your OTP to reset your password is: <b>${otp}</b><br />Please note that it will expire in <b>10 minutes,</b>`;
-
-      await sendEmail(email, "Request for Password Reset", content);
-
+    if (!existingOtp) {
       return res
-        .status(200)
-        .send(new ApiResponse(200, null, "OTP resent successfully."));
+        .status(404)
+        .send(
+          new ApiResponse(
+            404,
+            null,
+            "Kindly request an OTP before attempting to verify."
+          )
+        );
     }
 
-    const { otp, expires } = TOTP.generate(env.OTP_SECRET, {
-      digits: 6,
-      period: 600,
-    }); // 10 minutes = 600 s
+    if (parseInt(otp) !== parseInt(existingOtp.code)) {
+      return res.status(400).send(new ApiResponse(400, null, "Invalid OTP."));
+    }
 
-    await OTP.create({
-      code: otp,
-      email,
-      expiry: expires,
-    });
+    const hashed = await bcrypt.hash(newPassword, 10);
 
-    const content = `Dear User,<br /><br />We have received a request to reset your password for <b>Athena AI</b><br/>If this request was not made by you, you can ignore this email.<br /><br />Your OTP to reset your password is: <b>${otp}</b><br />Please note that it will expire in <b>10 minutes,</b>`;
+    existingUser.password = hashed;
+    await existingUser.save();
 
-    await sendEmail(email, "Request for Password Reset", content);
+    await existingOtp.deleteOne();
 
     res
-      .status(201)
-      .send(
-        new ApiResponse(201, null, "OTP sent to provided email successfully.")
-      );
+      .status(200)
+      .send(new ApiResponse(200, null, "Password updated successfully."));
   } catch (error) {
     console.log(error);
     res
       .status(500)
-      .send(new ApiResponse(500, error, "Failed to send reset link."));
+      .send(new ApiResponse(500, error, "Failed to reset password."));
   }
 };
 
-export default sendMail;
+export default resetPassword;
